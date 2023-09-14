310c1479b74248b9bd4ea1bb1a71b509c4dd6f7f
BuildTools <j.duquee77@gmail.com>
2023-09-14 13:54:12 +0100

    Ported mod to Forge

--- addon-templatinator/src/main.ts
+++
@@ -1,8 +0,0 @@
-import { createApp } from 'vue'
-import App from './App.vue'
-import router from './router'
-
-import './theme/global.scss'
-import './theme/theme.scss'
-
-createApp(App).use(router).mount('#app')
