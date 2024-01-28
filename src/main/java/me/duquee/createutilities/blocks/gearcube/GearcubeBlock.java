2616dd6e7dc3effbc83fd9834e69d189539cd690
BuildTools <j.duquee77@gmail.com>
2024-01-28 04:26:21 +0000

    Fixed shape

--- src/main/java/me/duquee/createutilities/blocks/gearcube/GearcubeBlock.java
+++ src/main/java/me/duquee/createutilities/blocks/gearcube/GearcubeBlock.java
@@ -12,15 +12,24 @@ import net.minecraft.world.entity.player.Player;
 import net.minecraft.world.item.ItemStack;
 import net.minecraft.world.level.BlockGetter;
 import net.minecraft.world.level.LevelReader;
+import net.minecraft.world.level.block.Block;
 import net.minecraft.world.level.block.entity.BlockEntityType;
 import net.minecraft.world.level.block.state.BlockState;
 import net.minecraft.world.phys.HitResult;
+import net.minecraft.world.phys.shapes.CollisionContext;
+import net.minecraft.world.phys.shapes.VoxelShape;
 
 public class GearcubeBlock extends KineticBlock implements IBE<GearboxBlockEntity>, BlockPickInteractionAware {
 
+	private static final VoxelShape SHAPE = Block.box(1, 1, 1, 15, 15, 15);
 
 	public GearcubeBlock(Properties properties) {
 		super(properties);
+	}
+
+	@Override
+	public VoxelShape getShape(BlockState state, BlockGetter level, BlockPos pos, CollisionContext context) {
+		return SHAPE;
 	}
 
 	@Override
