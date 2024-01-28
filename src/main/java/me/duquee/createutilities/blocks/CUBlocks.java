543ed3204670272cb50f99eeade50771d37e652a
BuildTools <j.duquee77@gmail.com>
2024-01-28 21:10:00 +0000

    Added the L-Shaped Gearbox

--- src/main/java/me/duquee/createutilities/blocks/CUBlocks.java
+++ src/main/java/me/duquee/createutilities/blocks/CUBlocks.java
@@ -1,11 +1,18 @@
 package me.duquee.createutilities.blocks;
 
+import com.simibubi.create.AllSpriteShifts;
+import com.simibubi.create.Create;
 import com.simibubi.create.content.decoration.encasing.CasingBlock;
+import com.simibubi.create.content.decoration.encasing.EncasedCTBehaviour;
 import com.simibubi.create.content.kinetics.BlockStressDefaults;
+import com.simibubi.create.content.kinetics.gearbox.GearboxBlock;
+import com.simibubi.create.foundation.data.AssetLookup;
 import com.simibubi.create.foundation.data.BuilderTransformers;
+import com.simibubi.create.foundation.data.CreateRegistrate;
 import com.simibubi.create.foundation.data.SharedProperties;
 import com.tterrag.registrate.util.entry.BlockEntry;
 
+import me.duquee.createutilities.blocks.lgearbox.LShapedGearboxBlock;
 import me.duquee.createutilities.blocks.voidtypes.chest.VoidChestBlock;
 import me.duquee.createutilities.blocks.voidtypes.motor.VoidMotorBlock;
 import me.duquee.createutilities.blocks.gearcube.GearcubeBlock;
@@ -13,11 +20,14 @@ import me.duquee.createutilities.blocks.voidtypes.tank.VoidTankBlock;
 import me.duquee.createutilities.tabs.CUCreativeTabs;
 import net.fabricmc.fabric.api.itemgroup.v1.ItemGroupEvents;
 import net.minecraft.client.renderer.RenderType;
+import net.minecraft.core.Direction;
 import net.minecraft.world.item.Rarity;
 import net.minecraft.world.level.block.SoundType;
 import net.minecraft.world.level.block.state.BlockBehaviour;
 import net.minecraft.world.level.material.MapColor;
+import net.minecraft.world.level.material.PushReaction;
 
+import static com.simibubi.create.foundation.data.BlockStateGen.axisBlock;
 import static com.simibubi.create.foundation.data.ModelGen.customItemModel;
 import static com.simibubi.create.foundation.data.TagGen.axeOrPickaxe;
 import static com.simibubi.create.foundation.data.TagGen.pickaxeOnly;
@@ -77,6 +87,18 @@ public class CUBlocks {
 			.simpleItem()
 			.register();
 
+	public static final BlockEntry<LShapedGearboxBlock> LSHAPED_GEARBOX = REGISTRATE.block("lshaped_gearbox", LShapedGearboxBlock::new)
+			.initialProperties(SharedProperties::stone)
+			.properties(BlockBehaviour.Properties::noOcclusion)
+			.properties(p -> p.mapColor(MapColor.PODZOL))
+			.transform(BlockStressDefaults.setNoImpact())
+			.transform(axeOrPickaxe())
+			.onRegister(CreateRegistrate.connectedTextures(() -> new EncasedCTBehaviour(AllSpriteShifts.ANDESITE_CASING)))
+			.onRegister(CreateRegistrate.casingConnectivity((block, cc) -> cc.make(block, AllSpriteShifts.ANDESITE_CASING,
+					(state, face) -> !LShapedGearboxBlock.hasShaftTowards(state, face))))
+			.simpleItem()
+			.register();
+
 	static {
 		ItemGroupEvents.modifyEntriesEvent(CUCreativeTabs.BASE.key()).register(content -> {
 			content.accept(VOID_CASING);
@@ -84,6 +106,7 @@ public class CUBlocks {
 			content.accept(VOID_CHEST);
 			content.accept(VOID_TANK);
 			content.accept(GEARCUBE);
+			content.accept(LSHAPED_GEARBOX);
 		});
 	}
 
