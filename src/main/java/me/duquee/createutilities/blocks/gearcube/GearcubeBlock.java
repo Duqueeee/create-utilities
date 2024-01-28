50bd06ddfef1a4bd0fe24fd0afd18a6ec857435f
BuildTools <j.duquee77@gmail.com>
2024-01-28 04:26:21 +0000

    Fixed shape

--- src/main/java/me/duquee/createutilities/blocks/gearcube/GearcubeBlock.java
+++ src/main/java/me/duquee/createutilities/blocks/gearcube/GearcubeBlock.java
@@ -3,18 +3,29 @@ package me.duquee.createutilities.blocks.gearcube;
 import com.simibubi.create.content.kinetics.base.KineticBlock;
 import com.simibubi.create.content.kinetics.gearbox.GearboxBlockEntity;
 import com.simibubi.create.foundation.block.IBE;
+
 import me.duquee.createutilities.blocks.CUTileEntities;
 import net.minecraft.core.BlockPos;
 import net.minecraft.core.Direction;
+import net.minecraft.world.level.BlockGetter;
 import net.minecraft.world.level.LevelReader;
+import net.minecraft.world.level.block.Block;
 import net.minecraft.world.level.block.entity.BlockEntityType;
 import net.minecraft.world.level.block.state.BlockState;
+import net.minecraft.world.phys.shapes.CollisionContext;
+import net.minecraft.world.phys.shapes.VoxelShape;
 
 public class GearcubeBlock extends KineticBlock implements IBE<GearboxBlockEntity> {
 
+	private static final VoxelShape SHAPE = Block.box(1, 1, 1, 15, 15, 15);
 
 	public GearcubeBlock(Properties properties) {
 		super(properties);
+	}
+
+	@Override
+	public VoxelShape getShape(BlockState state, BlockGetter level, BlockPos pos, CollisionContext context) {
+		return SHAPE;
 	}
 
 	@Override
